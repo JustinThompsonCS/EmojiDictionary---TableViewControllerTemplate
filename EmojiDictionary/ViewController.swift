b7d9c53b7ae8cc27db7b30c961e728ba987fe716
Justin Thompson <justindthomps@gmail.com>
2020-02-21 10:47:26 -0600

    Initial Commit

---
+++ EmojiDictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiDictionary
+//
+//  Created by Thompson, Justin D on 2/21/20.
+//  Copyright © 2020 Thompson, Justin D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
